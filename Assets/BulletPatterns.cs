331365aba453f3915bce30140df551f74c65374e
blingerama <bdomalski@mail.pima.edu>
2016-02-16 20:20:21 -0700

    Added 3 new Bullet Patterns, all targeted

--- Assets/BulletPatterns.cs
+++ Assets/BulletPatterns.cs
@@ -10,18 +10,9 @@ public class BulletPatterns : MonoBehaviour {
 
 	public GameObject gTar, ship;
 	public GameObject basicBullet;
+	private Transform gPos, sPos;
 
-	void Start(){circlePattern1(); }
-
-	private int testCounter = 0;
-
-	void Update(){
-
-		if (testCounter == 30)
-			circlePattern2 ();
-
-		testCounter++;
-	}
+	void Start(){gPos = gTar.GetComponent<Transform>(); sPos = ship.GetComponent<Transform>();}
 
 	void spawnBullet(float xDir, float yDir, GameObject bulletType){
 
@@ -29,10 +20,9 @@ public class BulletPatterns : MonoBehaviour {
 		BasicBulletMovement y = x.GetComponent<BasicBulletMovement> ();
 		y.xDir = xDir;
 		y.yDir = yDir;
-
 	}
 
-	public void circlePattern1(){
+	public void CirclePattern1(){
 
 		spawnBullet (-1f, 0f, basicBullet);
 		spawnBullet (1f, 0f, basicBullet);
@@ -47,7 +37,7 @@ public class BulletPatterns : MonoBehaviour {
 		spawnBullet (0.5878f, -0.809f, basicBullet);
 	}
 
-	public void circlePattern2(){
+	public void CirclePattern2(){
 
 		spawnBullet (-0.9877f, -0.1564f, basicBullet);
 		spawnBullet (0.9877f, -0.1564f, basicBullet);
@@ -59,7 +49,54 @@ public class BulletPatterns : MonoBehaviour {
 		spawnBullet (0.891f, -0.454f, basicBullet);
 		spawnBullet (-0.7071f, -0.7071f, basicBullet);
 		spawnBullet (0.7071f, -0.7071f, basicBullet);
+	}
 
+	public void TargetedPattern1(){
+
+		Vector3 normal = new Vector3 (sPos.x - gPos.x, sPos.y - gPos.y);
+		normal = normal / normal.magnitude;
+		float angle = Mathf.Asin (normal.y);
+		spawnBullet (Mathf.cos (angle + 16), Mathf.sin (angle + 16), basicBullet);
+		spawnBullet (Mathf.cos (angle - 16), Mathf.sin (angle - 16), basicBullet);
+		spawnBullet (normal.x, normal.y, basicBullet);
+	}
+
+	public void TargetedPattern2(){
+
+		Vector3 normal = new Vector3 (sPos.x - gPos.x, sPos.y - gPos.y);
+		normal = normal / normal.magnitude;
+		float angle = Mathf.Asin (normal.y);
+		spawnBullet (Mathf.Cos (angle + 8), Mathf.Sin (angle + 8), basicBullet);
+		spawnBullet (Mathf.Cos (angle - 8), Mathf.Sin (angle - 8), basicBullet);
+		spawnBullet (Mathf.Cos (angle + 24), Mathf.Sin (angle + 24), basicBullet);
+		spawnBullet (Mathf.Cos (angle - 24), Mathf.Sin (angle - 24), basicBullet);
+	}
+
+	public void TargetedPattern3(){
+
+		float root3Side = Mathf.Sin (60f) * 2f;
+		TargetedPattern3BulletSpawner (sPos.x + 2, sPos.y, basicBullet);
+		TargetedPattern3BulletSpawner (sPos.x + root3Side, sPos.y + 1, basicBullet);
+		TargetedPattern3BulletSpawner (sPos.x + 1, sPos.y + root3Side, basicBullet);
+		TargetedPattern3BulletSpawner (sPos.x, sPos.y + 2, basicBullet);
+		TargetedPattern3BulletSpawner (sPos.x - root3Side, sPos.y + 1, basicBullet);
+		TargetedPattern3BulletSpawner (sPos.x - 1, sPos.y + root3Side, basicBullet);
+		TargetedPattern3BulletSpawner (sPos.x - 2, sPos.y, basicBullet);
+		TargetedPattern3BulletSpawner (sPos.x - root3Side, sPos.y - 1, basicBullet);
+		TargetedPattern3BulletSpawner (sPos.x - 1, sPos.y - root3Side, basicBullet);
+		TargetedPattern3BulletSpawner (sPos.x, sPos.y - 2, basicBullet);
+		TargetedPattern3BulletSpawner (sPos.x + root3Side, sPos.y - 1, basicBullet);
+		TargetedPattern3BulletSpawner (sPos.x + 1, sPos.y - root3Side, basicBullet);
+	}
+
+	void TargetedPattern3BulletSpawner(float xPos, float yPos, GameObject bulletType){
+
+		Vector3 normal = new Vector3 (sPos.x - xPos, sPos.y - yPos);
+		normal = normal / normal.magnitude;
+		GameObject x = (GameObject)Instantiate (bulletType, new Vector3 (xPos, yPos), Quaternion.identity);
+		BasicBulletMovement y = x.GetComponent<BasicBulletMovement> ();
+		y.xDir = normal.x;
+		y.yDir = normal.y;
 	}
 
 }
