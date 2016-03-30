87afaa1d751500e458a5a227fee76196b5593b78
blingerama <bdomalski@mail.pima.edu>
2016-03-30 11:01:36 -0700

    Added a movement script for bullets moving in a spiral, not used yet

---
+++ Assets/Scripts/SpiralBulletMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+
+public class SpiralBulletMovement : MonoBehaviour {
+
+
+	public float theta, movementSpeed, ROCTheta;
+	public Transform transform; 
+
+	void Update(){
+
+		transform.Translate(new Vector3(Mathf.Cos(theta), Mathf.Sin(theta)) * Time.deltaTime * movementSpeed);
+
+		theta += Time.deltaTime * ROCTheta;
+				
+
+		if(transform.position.x <  -10 || transform.position.x > 10 || transform.position.y < -10 || transform.position.y > 10)
+			Destroy(gameObject);
+
+	}
+
+
+}
